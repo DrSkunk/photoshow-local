70d0858dfed0417e1632fc59872bc20dd89666db
Sebastiaan Jansen <contact@sebastiaanjansen.be>
2026-06-04 11:22:22 +0200

    feat: add title tag to app and layout for improved SEO

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,7 @@
 </script>
 
 <svelte:head>
+	<title>PhotoShow</title>
 	<meta name="theme-color" content="#0f172a" />
 	<link rel="manifest" href="/manifest.webmanifest" />
 	<link rel="apple-touch-icon" href="/icon.svg" />
